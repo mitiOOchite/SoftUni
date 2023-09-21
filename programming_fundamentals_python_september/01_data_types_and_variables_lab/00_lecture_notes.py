aa3eafc211c481c3b96fe89369e1184b6fc0ade5
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-09-21 16:34:41 +0300

    Commit1

--- programming_fundamentals_python_september/01_data_types_and_variables_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/01_data_types_and_variables_lab/00_lecture_notes.py
@@ -1,6 +1,11 @@
 # lst = ['mercedes', 'bmw', 'audi']
 # for i in range(len(lst)):
 #     print(i)
-var = complex(5, 7)
-print('Output:', var)
+# var = complex(5, 7)
+# print('Output:', var)
 #Output: (5+7j)
+from decimal import Decimal
+a = Decimal('0.1')
+b = Decimal('0.1')
+c = Decimal('0.1')
+print(a+b+c)
