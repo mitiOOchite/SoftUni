a67731e4b3378e02ab6b9d300e3b3dea4492d89b
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-09-22 10:28:37 +0300

    Commit1

--- programming_fundamentals_python_september/01_data_types_and_variables_lab/05_special_number.py
+++ programming_fundamentals_python_september/01_data_types_and_variables_lab/05_special_number.py
@@ -1,12 +1,18 @@
 number = int(input())
 SPECIAL_NUMBER = [5, 7, 11]
 for num in range(1, number + 1):
-    sum_of_digits = 0
-    digits = num
-    while digits > 0:
-        sum_of_digits += digits % 10
-        digits = int(digits / 10)
-    if sum_of_digits in SPECIAL_NUMBER:
-        print(f'{num} -> True')
-    else:
-        print(f'{num} -> False')
+    digits = str(num)
+    sum_dig = 0
+    for digit in digits:
+        sum_dig += int(digit)
+    isSpecial = False
+    if sum_dig in SPECIAL_NUMBER:
+        isSpecial = True
+    print(num,isSpecial)
+
+
+    # if sum_of_digits in SPECIAL_NUMBER:
+    #     print(f'{num} -> True')
+    # else:
+    #     print(f'{num} -> False')
+# -41:28
