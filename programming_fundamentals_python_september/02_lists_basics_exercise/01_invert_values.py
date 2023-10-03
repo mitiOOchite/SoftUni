38d2d3b937c715e74dcb54ee7332d53344288647
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-10-03 19:44:33 +0300

    Push changes

---
+++ programming_fundamentals_python_september/02_lists_basics_exercise/01_invert_values.py
@@ -0,0 +1,6 @@
+list_with_numbers = input().split()
+opposite_numbers = []
+for number in list_with_numbers:
+    current_number = -int(number)
+    opposite_numbers.append(current_number)
+print(opposite_numbers)
---
+++ programming_fundamentals_python_september/02_lists_basics_exercise/02_multiples_list.py
@@ -0,0 +1,6 @@
+factor = int(input())
+count = int(input())
+list = []
+for multiplier in range(1, count+1):
+    list.append(factor*multiplier)
+print(list)
