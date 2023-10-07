1c4197a6f6462d1bb31d92ba05cee408158d0f66
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-10-07 12:51:10 +0300

    Push changes

---
+++ programming_fundamentals_python_september/03_functions/00_lecture_notes.py
@@ -0,0 +1,12 @@
+def sum_numbers():
+    a = input()
+    b = input()
+    print(a+b)
+sum_numbers()
+
+#
+# for _ in range(3):
+#     a = int(input())
+#     b = int(input())
+#     result = sum_numbers(a,b)
+#     print(result)
