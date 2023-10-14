06b13b374a2449f1a416bcba561500218cd9ae09
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-10-14 11:34:28 +0300

    Push changes

--- programming_fundamentals_python_september/04_lists_advanced_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/04_lists_advanced_lab/00_lecture_notes.py
@@ -1,4 +1,18 @@
-nums = [1, 2, 3, 4, 5]
+nums = [1, 2, 2, 3, 3, 3, 4, 5]
 
 squares = [num for num in nums if num % 2 == 0]
 print(squares)
+
+letters = ['a', 'b', 'c', 'd']
+
+uppercase = [letter.upper() for letter in letters]
+print(uppercase)
+
+squared_dict = {num: num ** 2 for num in nums}
+print(squared_dict)
+
+even_set = {num for num in nums if num % 2 == 0}
+print(even_set)
+
+result = [num ** 2 if num % 2 == 0 else num ** 3 for num in nums]
+print(result)
