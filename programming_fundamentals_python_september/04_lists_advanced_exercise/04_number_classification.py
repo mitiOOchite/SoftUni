bf886608a5f4009eda09f1d5f4054f09c9f61986
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-10-15 13:08:24 +0300

    Push changes

---
+++ programming_fundamentals_python_september/04_lists_advanced_exercise/04_number_classification.py
@@ -0,0 +1,5 @@
+numbers = input().split(', ')
+print(f'Positive: {", ".join([number for number in numbers if int(number) >=0])}')
+print(f'Negative: {", ".join([number for number in numbers if int(number) < 0])}')
+print(f'Even: {", ".join([number for number in numbers if int(number) %2 == 0])}')
+print(f'Odd: {", ".join([number for number in numbers if int(number) %2 != 0])}')
