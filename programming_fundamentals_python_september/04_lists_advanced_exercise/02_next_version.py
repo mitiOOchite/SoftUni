0b055733510bfd1001606b020102f7977bb12cc0
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-10-15 13:08:31 +0300

    Push changes

---
+++ programming_fundamentals_python_september/04_lists_advanced_exercise/02_next_version.py
@@ -0,0 +1,8 @@
+version = [int(digit) for digit in input().split('.')]
+version[-1] += 1
+for index in range(len(version) - 1, -1, -1):
+    if version[index] > 9:
+        version[index] = 0
+        if index - 1 >= 0:
+            version[index - 1] += 1
+print('.'.join(str(number) for number in version))
