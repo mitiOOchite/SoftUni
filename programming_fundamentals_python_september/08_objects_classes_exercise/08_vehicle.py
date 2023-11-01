c31440c4057ca755865ddd7fd9a3e59de37b12e4
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-01 18:04:54 +0200

    Push changes

--- programming_fundamentals_python_september/08_objects_classes_exercise/08_vehicle.py
+++ programming_fundamentals_python_september/08_objects_classes_exercise/08_vehicle.py
@@ -5,11 +5,11 @@ class Vehicle:
         self.type = type
         self.owner = None
 
-    def buy(self, money, owner):
+    def buy(self, money:int, owner:str):
         if money >= self.price and self.owner is None:
             self.owner = owner
             change = money - self.price
-            return f'Succesfully bought a {self.type}. Change {change:.2f}'
+            return f'Successfully bought a {self.type}. Change: {change:.2f}'
         elif money < self.price:
             return 'Sorry, not enough money'
         else:
@@ -31,8 +31,7 @@ class Vehicle:
 vehicle_type = "car"
 model = "BMW"
 price = 30000
-vehicle = Vehicle(vehicle_type,
-                  model, price)
+vehicle = Vehicle(vehicle_type,model, price)
 print(vehicle.buy(15000, "Peter"))
 print(vehicle.buy(35000, "George"))
 print(vehicle)
