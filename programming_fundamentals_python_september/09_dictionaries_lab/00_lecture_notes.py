e5fbfc2e01f2ef97cf21575041668d2ee216e261
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-04 14:53:59 +0200

    Push changes

--- programming_fundamentals_python_september/09_dictionaries_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/09_dictionaries_lab/00_lecture_notes.py
@@ -10,12 +10,16 @@
 # student['name'] = input('Please input the new name: ') #this is how we can change the value within the dict
 # print(f'{student}, \n The students old name was: {old_name}')
 
-student_records = {
-    "Ivan":{"Math":5,"Science":6,"English":5},
-    "Mitko":{"Math":4,"Science":3,"English":2},
-    "Nikolay":{"Math":4,"Science":3,"English":2},
-}
-for name, value in student_records.items():
-    print(f'*** {name} ***')
-    for subject,score in value.items():
-        print(f'{subject} - {score}')
+# student_records = {
+#     "Ivan":{"Math":5,"Science":6,"English":5},
+#     "Mitko":{"Math":4,"Science":3,"English":2},
+#     "Nikolay":{"Math":4,"Science":3,"English":2},
+# }
+# for name, value in student_records.items():
+#     print(f'*** {name} ***')
+#     for subject,score in value.items():
+#         print(f'{subject} - {score}')
+
+fruits = ['apple', 'banana', 'mango']
+fruit_length = {fruit: len(fruit) for fruit in fruits}
+print(fruit_length)
