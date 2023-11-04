98bcdbed7f975ce46d857e8387be1e18a559921d
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-04 13:10:19 +0200

    Push changes

--- programming_fundamentals_python_september/09_dictionaries_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/09_dictionaries_lab/00_lecture_notes.py
@@ -1,1 +1,11 @@
-user_cred = {'user1': 'hashed_pass', 'user2': 'hashed_pass'}
+keys = ['name','age','major']
+values = ['Ivan','33','Computer Science']
+student = {}
+
+for i in range(len(keys)):
+    key = keys[i]
+    value = values[i]
+    student[key] = value
+old_name = student.get('name')
+student['name'] = input('Please input the new name: ') #this is how we can change the value within the dict
+print(f'{student}, \n The students old name was: {old_name}')
--- programming_fundamentals_python_september/09_dictionaries_lab/01_bakery.py
+++ programming_fundamentals_python_september/09_dictionaries_lab/01_bakery.py
@@ -5,6 +5,6 @@ for i in range(0, len(elements),2):
     key = elements[i]
     value = elements[i + 1]
     bakery[key] = int(value)
-
+for key in bakery.values():
+    print(key)
 print(bakery)
-
