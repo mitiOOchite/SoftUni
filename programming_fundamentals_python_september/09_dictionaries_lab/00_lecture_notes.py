a9b50f5c0b1e66761387e2ec8c1906358795dbee
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-04 14:25:52 +0200

    Push changes

--- programming_fundamentals_python_september/09_dictionaries_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/09_dictionaries_lab/00_lecture_notes.py
@@ -1,11 +1,21 @@
-keys = ['name','age','major']
-values = ['Ivan','33','Computer Science']
-student = {}
+# keys = ['name','age','major']
+# values = ['Ivan','33','Computer Science']
+# student = {}
+#
+# for i in range(len(keys)):
+#     key = keys[i]
+#     value = values[i]
+#     student[key] = value
+# old_name = student.get('name')
+# student['name'] = input('Please input the new name: ') #this is how we can change the value within the dict
+# print(f'{student}, \n The students old name was: {old_name}')
 
-for i in range(len(keys)):
-    key = keys[i]
-    value = values[i]
-    student[key] = value
-old_name = student.get('name')
-student['name'] = input('Please input the new name: ') #this is how we can change the value within the dict
-print(f'{student}, \n The students old name was: {old_name}')
+student_records = {
+    "Ivan":{"Math":5,"Science":6,"English":5},
+    "Mitko":{"Math":4,"Science":3,"English":2},
+    "Nikolay":{"Math":4,"Science":3,"English":2},
+}
+for name, value in student_records.items():
+    print(f'*** {name} ***')
+    for subject,score in value.items():
+        print(f'{subject} - {score}')
