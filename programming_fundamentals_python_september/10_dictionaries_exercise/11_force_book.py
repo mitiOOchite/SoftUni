5e2ae982b2b3a325dae5575f05df60fdf81ece5b
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-05 16:09:43 +0200

    Push changes

--- programming_fundamentals_python_september/10_dictionaries_exercise/11_force_book.py
+++ programming_fundamentals_python_september/10_dictionaries_exercise/11_force_book.py
@@ -17,6 +17,11 @@ while command != "Lumpawaroo":
         for value in force_side_dictionary.values():
             if force_user in value:
                 value.remove(force_user)
-            force_side_dictionary[force_side].apped(force_user)
-        print(f"{force_user} joins the {force_side} side!)
+            force_side_dictionary[force_side].append(force_user)
+        print(f"{force_user} joins the {force_side} side!")
     command = input()
+for force_side, force_users in force_side_dictionary.items():
+    if len(force_users)  >0:
+        print(f'Side: {force_side}, Members: {len(force_users)}')
+        for force_user in force_users:
+            print(f"! {force_user}")
