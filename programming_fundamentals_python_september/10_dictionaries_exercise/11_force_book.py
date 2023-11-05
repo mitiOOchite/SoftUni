53538b73ea682c38bc6e02d025d63537d8a58f1a
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-05 16:04:12 +0200

    Push changes

--- programming_fundamentals_python_september/10_dictionaries_exercise/11_force_book.py
+++ programming_fundamentals_python_september/10_dictionaries_exercise/11_force_book.py
@@ -14,7 +14,9 @@ while command != "Lumpawaroo":
             force_side_dictionary[force_side].append(force_user)
     elif "->" in command:
         force_user, force_side = command.split(" -> ")
-
-
+        for value in force_side_dictionary.values():
+            if force_user in value:
+                value.remove(force_user)
+            force_side_dictionary[force_side].apped(force_user)
         print(f"{force_user} joins the {force_side} side!)
     command = input()
