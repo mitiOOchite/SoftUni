c218bc86e42fad55697283013bd2ed9f3e53890f
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-18 17:00:02 +0200

    Push changes

--- programming_fundamentals_python_september/13_regex_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/13_regex_lab/00_lecture_notes.py
@@ -10,13 +10,25 @@ import re
 # regex = '\b\d{1,2}-[A-Z][a-z]{2}-\d{4}\b' validate date format dd-MMM-yyyy
 # text = input()
 # x = re.findall(regex,text)
-# print(x)
+# # print(x)
+#
+# mailregex = '^[a-zA-Z0-9_]+@+[a-zA-Z]+\.[a-zA-Z]+'
+# emails = ['valid123@mail.bg',
+# 'invalid*name@email.bg']
+# for email in emails:
+#     if re.match(mailregex, email):
+#         print(f'{email} is valid')
+#     else:
+#         print(f'{email} is invalid')
 
-mailregex = '^[a-zA-Z0-9_]+@+[a-zA-Z]+\.[a-zA-Z]+'
-emails = ['valid123@mail.bg',
-'invalid*name@email.bg']
-for email in emails:
-    if re.match(mailregex, email):
-        print(f'{email} is valid')
-    else:
-        print(f'{email} is invalid')
+import re
+string = 'The quick brown fox jumps over the lazy dog. Python is fun'
+match = re.search('\\bp\\w*',string,re.IGNORECASE) #ignores lower or uppercase
+if match:
+    print(f'The first word that starts with "p" is: {match.group()}')
+else:
+    print('No words starting with "p"')
+"""
+# if we use r'' before the regex we dont need to use escape characters 
+without them we need the double backslash
+"""
--- programming_fundamentals_python_september/13_regex_lab/01_match_full_names.py
+++ programming_fundamentals_python_september/13_regex_lab/01_match_full_names.py
@@ -1,6 +1,6 @@
 import re
 
 text = input()
-regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b"
+regex = r"\b[A-Z][a-z]+ [A-Z][a-z]+\b"
 matches = re.findall(regex, text)
 print(" ".join(matches))
