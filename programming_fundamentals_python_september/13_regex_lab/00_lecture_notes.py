3f1f1af555cfc9b26b8c8d70d50af544060b79a0
mitiOOchite <dimitartsvetkov26@protonmail.com>
2023-11-25 13:31:06 +0200

    Push changes

--- programming_fundamentals_python_september/13_regex_lab/00_lecture_notes.py
+++ programming_fundamentals_python_september/13_regex_lab/00_lecture_notes.py
@@ -8,11 +8,11 @@ import re
 #     print('False')
 
 # regex = '\b\d{1,2}-[A-Z][a-z]{2}-\d{4}\b' validate date format dd-MMM-yyyy
-# text = input()
-# x = re.findall(regex,text)
-# # print(x)
-#
-# mailregex = '^[a-zA-Z0-9_]+@+[a-zA-Z]+\.[a-zA-Z]+'
+# # text = input()
+# # x = re.findall(regex,text)
+# # # print(x)
+# #
+# # mailregex = '^[a-zA-Z0-9_]+@+[a-zA-Z]+\.[a-zA-Z]+'
 # emails = ['valid123@mail.bg',
 # 'invalid*name@email.bg']
 # for email in emails:
